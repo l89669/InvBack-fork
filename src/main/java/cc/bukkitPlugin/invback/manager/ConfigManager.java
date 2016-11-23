761c1c1193f8ea47a7d4f87e80f9da9357c27973
q178666380 <178666380@qq.com>
2016-11-23 12:45:29 +0800

    去除无用的配置

--- src/main/java/cc/bukkitPlugin/invback/manager/ConfigManager.java
+++ src/main/java/cc/bukkitPlugin/invback/manager/ConfigManager.java
@@ -57,8 +57,9 @@ public class ConfigManager extends AConfigManager<InvBack>{
         String tVersion=this.mConfig.getString(SEC_CFG_VERSION,"1.0");
         if(tVersion.compareTo("1.0")==0){
             tVersion="1.1";
-            this.mConfig.set("PlayerDataDir",null);
-            this.mConfig.set("FileNameMode",null);
+            this.mConfig.remove("PlayerDataDir");
+            this.mConfig.remove("FileNameMode");
+            this.mConfig.remove("ClearMemoryDataWherPlayerQuit");
         }
         this.mConfig.set(SEC_CFG_VERSION,this.mVersion);
         return true;
@@ -69,7 +70,6 @@ public class ConfigManager extends AConfigManager<InvBack>{
         this.mConfig.addDefault("BackupDir","back"+File.separator,"玩家个人数据备份位置","相对路径默认在插件文件夹下,也可以设置绝对路径");
         this.mConfig.addDefault("BackupInterval",900,"多少秒备份一次玩家数据");
         this.mConfig.addDefault("BackupExpriedDays",7,"玩家备份数据最长保留天数");
-        this.mConfig.addDefault("ClearMemoryDataWherPlayerQuit",true,"玩家退出时,清理对应玩家内存中的数据备份");
         this.mConfig.addDefault("KeepGameModeWhenRollBack",true,"在使用copy,set,rollback命令时保持还原数据前的游戏模式");
         this.mConfig.addDefault("ReplaceFileDataWithOnlineData",true,"如果对应的玩家在线,使用内存中的数据进行备份,而非模块对应的存档文件","启用此项可以比较实时的备份玩家数据");
         this.mConfig.addDefault("RemoveDataWhenPlayerQuit",true,"在玩家退出游戏时,清理该玩家在内存中的备份数据");
